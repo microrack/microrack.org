f13f0fb3634870460edb34c7a721e2fd2b121f24
Leonid Gomberg <forhotchpotch@gmail.com>
2024-03-12 23:20:23 +0400

    Add minor fixes

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -2,5 +2,5 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "MICRORACK - The most accessible synthesizer",
-  description: "Build unique instruments, while learning about synthesis step by step with highly accessible and affordable kits and simple design for all kinds of users. Customize your setup and combine with anything you can imagine. Feel free to collaborate, contribute and implement new ideas"
+  description: "Accessible modular analog synthesizer. Combine dozens of modules, play unique instruments, experiment and have fun! Build unique instruments, while learning about synthesis step by step with highly accessible and affordable kits and simple design for all kinds of users. Customize your setup and combine with anything you can imagine. Feel free to collaborate, contribute and implement new ideas"
 };
