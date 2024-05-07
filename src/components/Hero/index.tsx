89f1a7d736bf622efb09317bb66eeef2c279181b
Leonid Gomberg <forhotchpotch@gmail.com>
2024-05-07 18:37:30 +0400

    Separate content from layout

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,49 +12,103 @@ import styles from "./hero.module.css";
 
 import {HeroProps} from './hero.types'
 import image from "./assets/cover-1.jpg";
+import texts from "./text.json";
+
+const OPT_IN_URL = "https://forms.gle/CgNbhLSbxbahZK8BA";
 
 export const Hero: React.FC<HeroProps> = ({className}: HeroProps) => {
 	const dotSize = 4;
 	const dotSpacing = 15;
 
 	return (
-    <Container className={classnames(styles.container, className)}>
-      <Item columns={1} hideOnMobile hideOnTablet>
-        <Dots dotSize={dotSize} dotSpacing={dotSpacing} />
+    <Container className={
+      classnames(styles.container, className)
+    }>
+      <Item
+        columns={1}
+        hideOnMobile
+        hideOnTablet
+      >
+        <Dots
+          dotSize={dotSize}
+          dotSpacing={dotSpacing}
+        />
       </Item>
-      <Item columns={2} hideOnMobile>
-        <Dots dotSize={dotSize} dotSpacing={dotSpacing} />
+      <Item
+        columns={2}
+        hideOnMobile
+      >
+        <Dots
+          dotSize={dotSize}
+          dotSpacing={dotSpacing}
+        />
       </Item>
 
 
-      <Item columns={3} className={styles.content}>
-        <Dots dotSize={dotSize} dotSpacing={dotSpacing} height=""/>
-        <Text as="h1" weight="bold" size="xl" className={styles.description}>
-          The most accessible modular synthesizer
+      <Item
+        columns={3}
+        className={styles.content}
+      >
+        <Dots
+          height=""
+          dotSize={dotSize}
+          dotSpacing={dotSpacing}
+        />
+        <Text
+          as="h1"
+          size="xl"
+          weight="bold"
+          className={styles.description}
+        >
+          {texts.heroTitle}
         </Text>
-        <Text size="m" className={styles.description}>
-          Combine dozens of modules, build unique instruments, experiment with sound and have fun!
+        <Text
+          size="m"
+          className={styles.description}
+        >
+          {texts.heroDescription}
         </Text>
         <Button
-          className={styles.button}
-          href="https://forms.gle/CgNbhLSbxbahZK8BA"
           variant="action"
-          text="Join the waitlist"
+          href={OPT_IN_URL}
+          text={texts.heroButton}
+          className={styles.button}
         />
         
-        <Dots dotSize={dotSize} dotSpacing={dotSpacing} height=""/>
+        <Dots
+          height=""
+          dotSize={dotSize}
+          dotSpacing={dotSpacing}
+        />
       </Item>
 
 
-      <Item columns={1} hideOnMobile>
-        <Dots dotSize={dotSize} dotSpacing={dotSpacing} />
+      <Item
+        columns={1}
+        hideOnMobile
+      >
+        <Dots
+          dotSize={dotSize}
+          dotSpacing={dotSpacing}
+        />
       </Item>
-      <Item columns={4} className={styles.imageItem}>
-        <Image src={image} alt="MicroRack" fill={true} className={styles.imageComponent} />
+      <Item
+        columns={4}
+        className={styles.imageItem}
+      >
+        <Image
+          fill={true}
+          src={image}
+          className={styles.imageComponent}
+          alt={texts.imgAlt}
+        />
         {/* <Dots dotSize={dotSize} dotSpacing={dotSpacing} /> */}
       </Item>
       <Item columns={1}>
-        <Dots dotSize={dotSize} dotSpacing={dotSpacing} />
+        <Dots
+          dotSize={dotSize}
+          dotSpacing={dotSpacing}
+        />
       </Item>
     </Container>
 	);
