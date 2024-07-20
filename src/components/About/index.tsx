18111e6fa68e5085031791d84e28da18144567c9
Leonid Gomberg <forhotchpotch@gmail.com>
2024-07-20 22:59:12 +0400

    Fix apos

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -16,7 +16,7 @@ export const About: React.FC<Props> = ({className}) => {
 			MICRORACK is <b>the most accessible</b> modular synthesizer placed right on the breadboard. It is offering an <b>impressive sound variability</b> while being  affordable and easy to learn.
 			</Text>
 			<Text size="xs" className={styles.text}>
-			MICRORACK is <b>super portable</b>. Its compact design, DC and <b>USB-C power</b> compatibility mean you can easily pack it in a bag and <b>take it anywhere</b>. Use a power bank to enjoy <b>modular synthesis on the go</b> - it`&apos;s like having a <b>portable rave!</b>. 
+			MICRORACK is <b>super portable</b>. Its compact design, DC and <b>USB-C power</b> compatibility mean you can easily pack it in a bag and <b>take it anywhere</b>. Use a power bank to enjoy <b>modular synthesis on the go</b> - it&apos;s like having a <b>portable rave!</b>. 
 			</Text>
 			<Text size="xs" className={styles.text}>
 				We want to make modular synthesis accessible and <b>not expensive</b>, while promoting education in sound and electronics. Our goal is to <b>lower the learning curve</b> of synthesis for newcomers by providing tutorials and <b>step-by-step guides</b>. 
